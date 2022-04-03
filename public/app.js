487683217ed7a16e167a43e4140083b6509554ca
Dyrits <dyrits@outlook.com>
2022-04-03 16:41:41 -0500

    fetch() corrected with the help of GitHub Copilot.

--- public/app.js
+++ public/app.js
@@ -10,17 +10,26 @@ app.configuration = {
 app.client = {};
 
 // Interface for making API calls:
-app.client.request = async function (headers, path, method, query, payload, callback) {
+app.client.request = function (headers, path, method, query, payload, callback) {
     headers = headers || {};
+    app.configuration.token && (headers.token = app.configuration.token)
     path = path || "/";
-    method = method.toUpperCase();
     query = query || {};
     let url = `${path}?`;
     for (const [key, value] in Object.entries(query)) { url += `${key}=${value}&`; }
-    headers['Content-Type'] = 'application/json';
-    app.configuration.token && (headers.token = app.configuration.token);
-    await fetch(url, {headers, method, body: payload})
-        .then(response => ({status: response.status, data: response.json()}))
-        .then(result => { callback(result.status, result.data); })
-        .catch(error => { callback(error.status, false); });
+    const options = {
+        method: method.toUpperCase(),
+        headers: { ...headers, "Content-Type": "application/json"  },
+        body: payload
+    };
+    fetch(url, options)
+        .then(response => {
+            if (response.status !== 200) { return response.json()
+                .then(data => { callback({ status: response.status, data: data }); });
+            }
+            return response.json().then(data => {callback({ status: 200, data });});
+        })
+        .catch(error => { callback({ status: 500, data: { error }}); });
 }
+
+
